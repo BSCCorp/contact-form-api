9edba3989b9f6329dc1891ab6d1ef964877e57cb
Daniel Bidulock <daniel@bscc.dev>
2026-09-01 18:14:46 -0600

    Launch maildev for tests

--- playwright.config.js
+++ playwright.config.js
@@ -12,10 +12,17 @@ module.exports = defineConfig({
     baseURL: "http://localhost:3000",
   },
 
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:3000/health",
-    reuseExistingServer: true,
-  },
+  webServer: [
+    {
+      command: "npm run start",
+      url: "http://localhost:3000/health",
+      reuseExistingServer: true,
+    },
+    {
+      command: "maildev --smtp 1025 --web 1080",
+      url: "http://127.0.0.1:1080",
+      reuseExistingServer: true,
+    },
+  ]
 });
 
